5af43e133e5cfbf5ef2e26bd32e1aa844333dd41
Martin Voelkle <Martin.Voelkle@unige.ch>
2025-09-25 17:30:19 +0200

    tar only

--- opt/entrypoint.sh
+++ opt/entrypoint.sh
@@ -8,8 +8,7 @@ do
     mkdir "$INDEX"
     pushd "$INDEX"
     curl --fail --location --remote-name "$URL"
-    for TAR_FILE in *.tar*; do ! test -f "$TAR_FILE" || tar x -a -f "$TAR_FILE"; done
-    for ZIP_FILE in *.zip; do ! test -f "$ZIP_FILE" || unzip -q "$ZIP_FILE"; done
+    for TAR_FILE in *.tar*; do tar x -a -f "$TAR_FILE"; done
     find -name build.xml -exec ant -Dbuild.dir="$PWD"/build -buildfile {} ';'
     find build -name '*.xar' -exec mv {} ../"$INDEX".xar ';'
     popd
