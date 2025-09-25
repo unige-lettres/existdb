b4011db24bb805372d27bca508b94bcb62acf400
Martin Voelkle <Martin.Voelkle@unige.ch>
2025-09-25 10:11:18 +0200

    busybox

--- opt/entrypoint.sh
+++ opt/entrypoint.sh
@@ -13,13 +13,13 @@ do
     mkdir "$INDEX"
     pushd "$INDEX"
     curl --fail --location --remote-name "$CONTEXT"
-    for TAR_FILE in *.tar*; do ! test -f "$TAR_FILE" || tar --extract --file "$TAR_FILE"; done
+    for TAR_FILE in *.tar*; do ! test -f "$TAR_FILE" || tar x -a -f "$TAR_FILE"; done
     for ZIP_FILE in *.zip; do ! test -f "$ZIP_FILE" || unzip -q "$ZIP_FILE"; done
     find -name build.xml -exec ant -Dbuild.dir="$PWD"/build -buildfile {} ';'
     find build -name '*.xar' -exec mv {} ../"$INDEX".xar ';'
     popd
-    rm --recursive "$INDEX"
+    rm -r "$INDEX"
     INDEX=$(("$INDEX"+1))
 done
 
-cp --recursive /opt existdb
+cp -r /opt existdb
